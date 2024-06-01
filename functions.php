15fb39d53c9f72f864222fc2795288199cd88bc2
coderRhythm <rhythmcoder966@gmail.com>
2024-06-01 18:19:50 +0530

    updated

--- functions.php
+++ functions.php
@@ -2,7 +2,23 @@
 
     echo "Welcome to the php tutorial on functions";
 
-    
+    function processMarks($marksArr)
+    {
+        $sum = 0;
+        foreach($marksArr as $i)
+        {
+            $sum += $i;
+        }
+
+        return $sum;
+    }
+
+
+    $rohanDas = [34,98,45,12,98,93];
+
+    $sumMarks = processMarks($rohanDas);
+
+    echo $sumMarks;
 
 
 
